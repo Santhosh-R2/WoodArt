6ea90dc147f403eef5a6238de2dc232578c02e70
Santhosh-R1 <santhoshrajan817@gmail.com>
2026-04-08 14:11:33 +0530

    push

--- user/src/utils/api.js
+++ user/src/utils/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:5000/api',
+  baseURL: 'https://wood-art-drab.vercel.app/api',
 });
 
 // Request interceptor to add auth token (Artisanal Identity Access Control)
